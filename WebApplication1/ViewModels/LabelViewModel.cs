bac46ab85c2441b523f58c0daef2598f88d3c345
ozitron <oguzfsen@gmail.com>
2023-10-09 22:48:54 +0300

    iş görüyor.. önemli olanda bu,

--- WebApplication1/ViewModels/LabelViewModel.cs
+++ WebApplication1/ViewModels/LabelViewModel.cs
@@ -8,10 +8,18 @@ namespace WebApplication1.ViewModels
     public class LabelViewModel
     {
         public int id { get; set; }
-        public int fotoID { get; set; }
+        public int photoID { get; set; }
         public int label { get; set; }
-        public int cursorCol { get; set; }
-        public int cursorRow { get; set; }
-        public int cursorSize { get; set; }
+        public Rectangle rectangle { get; set; }
+        public int sizeFactor { get; set; }
+    }
+
+    public class Rectangle
+    {
+        public int beginX { get; set; }
+        public int endX { get; set; }
+        public int beginY { get; set; }
+        public int endY { get; set; }
+
     }
 }
